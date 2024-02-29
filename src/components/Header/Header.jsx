6733613b2a21ebc02d63a723d8392463784972e8
eli-andrade-dev <elisangela.dev@hotmail.com>
2024-02-28 23:37:26 -0300

    primeiro commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Header.css"
+
+function Header(){
+  return (
+    <header>
+
+      <h2>Hamburgueria</h2>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Sobre nós</li>
+          <li>Contato</li>
+          <li>Lojas</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
